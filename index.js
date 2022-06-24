7b95ed661b153cc583a678780b1abb29575235d4
marylyne <leahmarylyne@gmail.com>
2022-06-24 12:03:17 +0300

    first push

---
+++ index.js
@@ -0,0 +1,19 @@
+let num1 = parseFloat(window.prompt("Enter a number"));
+const operator = (prompt('Enter operator ( either +, -, * or / ): '))
+let num3 = window.prompt("enter another number")
+
+//let result = parseFloat(window.prompt("result"))
+document.write;
+
+if (operator == '+') {
+    result = number1 + number2;
+} else if (operator == '-') {
+    result = number1 - number2;
+} else if (operator == '*') {
+    result = number1 * number2;
+} else {
+    result = number1 / number2;
+}
+
+
+console.log(`${number1} ${operator} ${number2} = ${result}`);
